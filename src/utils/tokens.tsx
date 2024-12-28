d194bd11d609fa518edd97658d22c7a992ace201
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-28 06:13:33 -0500

    feat(tokens): Create ts tokens
    - Create global tokens.
    - Create color tokens.
    - Create tokens file to manage all the available files with valid tokens.

---
+++ src/utils/tokens.tsx
@@ -0,0 +1,4 @@
+import { global } from "./tokens/global";
+import { color } from "./tokens/color";
+
+export { global, color }
---
+++ src/utils/tokens/color.tsx
@@ -0,0 +1,17 @@
+export const color = {
+	primary: {
+		base: '#396AFF',
+		light: '#E7EDFF',
+		dark: '#343C6A'
+	},
+	secondary: {
+		base: '#8BA3CB',
+		light: '#F5F7FA',
+		dark: '#718EBF'
+	},
+	mono: {
+		base: '#B1B1B1',
+		light: '#FFFFFF',
+		dark: '#232323'
+	}
+}
