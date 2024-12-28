91e58369973a47758e91e52ee2c2e908a44a5ae3
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-28 12:26:09 -0500

    fix: Configure app routing
    - Install “react-router-dom” package.
    - Install “@types/react-router-dom” package.
    - Implement React Router for simple navigation between pages.
    - Create “Dashboard” page.
    - Create “Settings” page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Header } from './ui/containers/Header'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './ui/pages/Dashboard'
+import Settings from './ui/pages/Settings'
+import { Link } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App: React.FC = () => {
+	return (
+		<>
+			{/* Navigation */}
+			<nav>
+				<ul>
+					<li><Link to="/">Dashboard</Link></li>
+					<li><Link to="/settings">Settings</Link></li>
+				</ul>
+			</nav>
 
-  return (
-    <>
-	<Header title="Overview" />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+			{/* Routes */}
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/settings" element={<Settings />} />
+			</Routes>
+		</>
+	)
 }
 
 export default App
