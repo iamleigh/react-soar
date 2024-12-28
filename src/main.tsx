c2dae9dc6beb453b2f94c920bd59065654f8d8c2
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-28 04:12:28 -0500

    feat: Install icons font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./assets/scss/main.scss";
+import "./assets/scss/soaricons.scss";
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
