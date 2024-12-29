8cb4842908c60f6d01f070a8e1ae7fa66689f653
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-29 13:23:08 -0500

    fix(dashboard): Load user cards

--- src/ui/pages/Dashboard.tsx
+++ src/ui/pages/Dashboard.tsx
@@ -1,10 +1,49 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import styled from "styled-components"
+import { global } from "../../utils/tokens"
 import { Page } from "../layouts/Page"
+import { Box } from "../components/Box"
+import { Card as UICard } from "../components/Card"
+import { CardGroup } from "../components/CardGroup"
+
+const Card = styled(UICard)`
+	flex: 0 0 auto;
+
+	&:not(:first-child) {
+		margin-left: 20px;
+
+		@media screen and (min-width: ${global.breakpoint}px) {
+			margin-left: 30px;
+		}
+	}
+`
 
 const Dashboard: React.FC = () => {
+	const [cards, setCards] = useState([]);
+
+	useEffect(() => {
+		fetch( '/api/card' )
+			.then((res) => res.json())
+			.then((data) => setCards(data))
+	}, [])
+
 	return (
 		<Page title="Dashboard" fullwidth={ true }>
-			<p>Welcome to my content</p>
+			<Box title="My Cards" path="/credit-cards" boxed={ false }>
+				<CardGroup>
+					{ cards && cards.map( ( card, index ) => {
+						return (
+							<Card
+								key={ index }
+								name={ card.name }
+								number={ card.number }
+								balance={ card.balance }
+								expiration={ card.expiration }
+								light={ card.light } />
+						)
+					}) }
+				</CardGroup>
+			</Box>
 		</Page>
 	)
 }
