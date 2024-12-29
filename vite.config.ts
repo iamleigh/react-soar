8278c55de19b5d993d1ef4a6bfe2af95dc1a25c1
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-29 13:40:23 -0500

    fix: Incorrect setup to resolve aliases

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,7 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@': './src',
-			'@styles': './src/assets/scss',
-			'@images': './src/assets/images',
-			'@utils': './src/utils',
-			'@components': './src/ui/components',
-			'@containers': './src/ui/containers',
-			'@layouts': './src/ui/layouts',
-			'@pages': './src/ui/pages',
+			'@': './src'
 		}
 	}
 })
