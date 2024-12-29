76f672ceff2d1ae9bde46e1c1a1e42a1a0bd70e9
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-29 17:09:48 -0500

    fix(dashboard): Add all required sections
    Use TailwindCSS to define the page content grid and place each Box element that will contain the required information based on the specs.

--- src/ui/pages/Dashboard.tsx
+++ src/ui/pages/Dashboard.tsx
@@ -29,21 +29,47 @@ const Dashboard: React.FC = () => {
 
 	return (
 		<Page title="Dashboard" fullwidth={ true }>
-			<Box title="My Cards" path="/credit-cards" boxed={ false }>
-				<CardGroup>
-					{ cards && cards.map( ( card, index ) => {
-						return (
-							<Card
-								key={ index }
-								name={ card.name }
-								number={ card.number }
-								balance={ card.balance }
-								expiration={ card.expiration }
-								light={ card.light } />
-						)
-					}) }
-				</CardGroup>
-			</Box>
+			<div className="flex flex-col lg:flex-row lg:space-x-[30px] mb-[22px]">
+				<Box title="My Cards" path="/credit-cards" boxed={ false } className="basis-full mb-[22px] lg:basis-8/12 lg:mb-0">
+					<CardGroup>
+						{ cards && cards.map( ( card, index ) => {
+							return (
+								<Card
+									key={ index }
+									name={ card.name }
+									number={ card.number }
+									balance={ card.balance }
+									expiration={ card.expiration }
+									light={ card.light } />
+							)
+						}) }
+					</CardGroup>
+				</Box>
+
+				<Box title="Recent Transactions" className="basis-full lg:basis-4/12">
+					Transactions go here
+				</Box>
+			</div>
+
+			<div className="flex flex-col lg:flex-row lg:space-x-[30px] mb-[22px]">
+				<Box title="Weekly Activity" className="basis-full mb-[22px] lg:basis-8/12 lg:mb-0">
+					<p>Content goes here</p>
+				</Box>
+
+				<Box title="Expense Statistics" className="basis-full lg:basis-4/12">
+					<p>Content goes here</p>
+				</Box>
+			</div>
+
+			<div className="flex flex-col lg:flex-row lg:space-x-[30px]">
+				<Box title="Quick Transfer" className="basis-full mb-[22px] lg:basis-4/12 lg:mb-0">
+					<p>Content goes here</p>
+				</Box>
+
+				<Box title="Balance History" className="basis-full lg:basis-8/12">
+					<p>Content goes here</p>
+				</Box>
+			</div>
 		</Page>
 	)
 }
