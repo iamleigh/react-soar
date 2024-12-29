b65dadd801091d49401a3319e21977a527a54429
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-29 02:46:15 -0500

    fix: Migrate project paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': './src',
+			'@styles': './src/assets/scss',
+			'@images': './src/assets/images',
+			'@utils': './src/utils',
+			'@components': './src/ui/components',
+			'@containers': './src/ui/containers',
+			'@layouts': './src/ui/layouts',
+			'@pages': './src/ui/pages',
+		}
+	}
 })
