9c0a29e4b49fe35e4faaf90584a2f06591972135
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-31 10:09:21 -0500

    feat(settings): Create new page

--- src/ui/pages/Settings.tsx
+++ src/ui/pages/Settings.tsx
@@ -1,10 +1,56 @@
 import React from "react"
+import styled from "styled-components"
+import { global, color, border } from "../../utils/tokens"
 import { Page } from "../layouts/Page"
+import { Tabs } from "../containers/Tabs"
+
+const Container = styled.div`
+	padding: 20px;
+	border-radius: ${ border.radius.md }px;
+	background: ${ color.mono.light };
+
+	@media screen and (min-width: ${global.breakpoint}px) {
+		padding: 30px;
+		border-radius: ${ border.radius.lg }px;
+	}
+`
 
 const Settings: React.FC = () => {
+	const editProfile = (
+		<p>Edit Profile content goes here</p>
+	)
+
+	const preferences = (
+		<p>Preferences content goes here</p>
+	)
+
+	const security = (
+		<p>Security content goes here</p>
+	)
+
+	const tabs = [
+		{
+			id: 'edit-profile',
+			label: 'Edit Profile',
+			content: editProfile
+		},
+		{
+			id: 'preferences',
+			label: 'Preferences',
+			content: preferences
+		},
+		{
+			id: 'security',
+			label: 'Security',
+			content: security
+		},
+	]
+
 	return (
 		<Page title="Settings">
-			<p>Welcome to my content</p>
+			<Container>
+				<Tabs datasets={ tabs } />
+			</Container>
 		</Page>
 	)
 }
