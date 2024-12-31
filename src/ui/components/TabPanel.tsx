06e1249ed7c9072d2b2686b06cf4181e64e91522
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-31 12:36:08 -0500

    fix(settings): Add content to page (#15)
    * feat(tab-button): Create new component
    
    * feat(tab-panel): Create new component
    
    * feat(tabs): Create new component
    
    * feat(settings): Create new page
    
    * fix(tabs): Incorrect prop
    
    * fix(tab-panel): Remove unused token
    
    * fix(settings): Update tabs content
    
    - Upload user avatar.
    - Upload empty avatar.
    - Update mock api for user data.
    - Update button props for inline variation.
    - Update input color.
    - Update input field to accept extended props.
    - Update input field styles for horizontal variation.
    - Update tab panel for left and right inner spacing.
    - Create “ProfileSettings” layout.
    - Add coming soon content to settings without design.
    - Add “ProfileSettings” content to its correspondent tab.

---
+++ src/ui/components/TabPanel.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import styled from 'styled-components'
+import { global } from '../../utils/tokens'
+
+const Container = styled.div<{ $isHidden?: boolean }>`
+	${ props => props.$isHidden && 'display: none;' }
+	padding: 45px 0;
+
+	@media screen and (min-width: ${global.breakpoint}px) {
+		padding: 40px 30px;
+	}
+`
+
+interface TabPanelProps extends React.PropsWithChildren<{}> {
+	id: string
+	selected?: boolean
+}
+
+export const TabPanel: React.FC<TabPanelProps> = ({
+	id,
+	selected = false,
+	children
+}) => {
+	return (
+		<Container
+			id={ `${ id }__panel` }
+			role="tabpanel"
+			aria-labelledby={ `${ id }__button` }
+			$isHidden={ !selected }>
+			{ children }
+		</Container>
+	)
+}
--- src/ui/pages/Settings.tsx
+++ src/ui/pages/Settings.tsx
@@ -1,10 +1,65 @@
 import React from "react"
+import styled from "styled-components"
+import { global, color, border } from "../../utils/tokens"
 import { Page } from "../layouts/Page"
+import { Tabs } from "../containers/Tabs"
+import { ProfileSettings } from "../layouts/ProfileSettings"
+
+const Container = styled.div`
+	padding: 20px;
+	border-radius: ${ border.radius.md }px;
+	background: ${ color.mono.light };
+
+	@media screen and (min-width: ${global.breakpoint}px) {
+		padding: 30px;
+		border-radius: ${ border.radius.lg }px;
+	}
+`
+
+const ComingSoon = styled.div`
+	width: 100%;
+	height: 200px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	background: ${ color.secondary.light };
+`
+
+const SoonTab: React.FC<{ title?: string }> = ({ title }) => {
+	return (
+		<ComingSoon>
+			<div style={{ textAlign: 'center' }}>
+				{ title && <p style={{ marginBottom: 10, fontWeight: '600' }}>{ title }</p> }
+				<p>Coming Soon</p>
+			</div>
+		</ComingSoon>
+	)
+}
 
 const Settings: React.FC = () => {
+	const tabs = [
+		{
+			id: 'edit-profile',
+			label: 'Edit Profile',
+			content: <ProfileSettings />
+		},
+		{
+			id: 'preferences',
+			label: 'Preferences',
+			content: <SoonTab title="Preferences" />
+		},
+		{
+			id: 'security',
+			label: 'Security',
+			content: <SoonTab title="Security" />
+		},
+	]
+
 	return (
 		<Page title="Settings">
-			<p>Welcome to my content</p>
+			<Container>
+				<Tabs datasets={ tabs } />
+			</Container>
 		</Page>
 	)
 }
