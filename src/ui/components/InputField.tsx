94bc3b41b6441e089c32e6628573d5e8822d609c
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-31 03:41:24 -0500

    feat(input-field): Create new component

---
+++ src/ui/components/InputField.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { global } from '../../utils/tokens'
+import { Input } from './Input'
+import { Label } from './Label'
+
+const Container = styled.div<{ $horizontal?: boolean }>`
+	${ props => props.$horizontal && 'display: flex;' }
+	${ props => props.$horizontal && 'flex-flow: row nowrap;' }
+	${ props => props.$horizontal && 'align-items: center;' }
+	margin: ${ props => props.$horizontal ? 29 : 16 }px 0 0;
+
+	&:first-child {
+		margin: 0;
+	}
+
+	label {
+		flex: 0 0 auto;
+		margin-right: 25px;
+
+		@media screen and (min-width: ${global.breakpoint}px) {
+			margin-right: 27px;
+		}
+	}
+`
+
+interface InputFieldProps {
+	id: string
+	label: string
+	placeholder?: string
+	solid?: boolean
+	horizontal?: boolean
+}
+
+export const InputField: React.FC<InputFieldProps> = ({
+	id,
+	label,
+	placeholder,
+	solid = false,
+	horizontal = false
+}) => {
+	return (
+		<Container $horizontal={ horizontal }>
+			<Label htmlFor={ id } value={ label } solid={ solid } />
+			<Input id={ id } placeholder={ placeholder } solid={ solid } />
+		</Container>
+	)
+}
