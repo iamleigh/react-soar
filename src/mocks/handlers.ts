6073cd494338e7278f6840644d6eeb0688e698ec
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2024-12-30 21:26:11 -0500

    fix(dashboard): Add expenses statistics chart (#9)
    * fix: Update mocked transactions
    
    - Add new transactions for the month.
    - Include category to each transaction.
    - Mock available categories (to be used later).
    
    * feat(pie-chart): Create new component
    
    * fix(transaction): Add new property for “category”
    
    * fix(bar-chart): Limit chart height
    
    * fix(dashboard): Display pie chart with expenses statistics
    
    * fix: Install “chartjs-plugin-datalabels” module
    
    This module will help the pie chart to display labels in the arcs.
    
    * fix(dashboard): Assign missing parameters

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -43,145 +43,262 @@ export const handlers = [
 	// Mock endpoint for transactions
 	http.get( '/api/transactions', () => {
 		return HttpResponse.json([
+			// 2 December
+			{
+				source: 'card',
+				amount: -1259,
+				date: '2024-12-1',
+				description: 'Cleaning service',
+				category: 'bill'
+			},
+			// 8 December
+			{
+				source: 'card',
+				amount: -625,
+				date: '2024-12-8',
+				description: 'Internet payment',
+				category: 'bill'
+			},
+			// 12 December
+			{
+				source: 'card',
+				amount: -183,
+				date: '2024-12-12',
+				description: 'Gasoline payment',
+				category: 'bill'
+			},
+			{
+				source: 'card',
+				amount: -216,
+				date: '2024-12-12',
+				description: 'Gasoline payment',
+				category: 'bill'
+			},
+			// 16 December
+			{
+				source: 'card',
+				amount: -356,
+				date: '2024-12-16',
+				description: 'Gasoline payment',
+				category: 'bill'
+			},
+			// 20 December
+			{
+				source: 'card',
+				amount: -152,
+				date: '2024-12-20',
+				description: 'Gasoline payment',
+				category: 'bill'
+			},
+			// 23 December
+			{
+				source: 'card',
+				amount: -374,
+				date: '2024-12-23',
+				description: 'Light service payment',
+				category: 'bill'
+			},
+			{
+				source: 'card',
+				amount: -162,
+				date: '2024-12-23',
+				description: 'Water service payment',
+				category: 'bill'
+			},
+			{
+				source: 'card',
+				amount: -64,
+				date: '2024-12-23',
+				description: 'Gasoline payment',
+				category: 'bill'
+			},
 			// 24 December
 			{
 				source: 'card',
 				amount: -896,
 				date: '2024-12-24',
-				description: 'Wallmart payment'
+				description: 'Wallmart payment',
+				category: 'other'
 			},
 			{
 				source: 'card',
 				amount: -918,
 				date: '2024-12-24',
-				description: 'Target payment'
+				description: 'Target payment',
+				category: 'other'
 			},
 			{
 				source: 'transfer',
 				amount: 780,
 				date: '2024-12-24',
-				description: 'Payment from Mick Jager'
+				description: 'Payment from Mick Jager',
+				category: 'income'
 			},
 			// 25 December
 			{
 				source: 'card',
 				amount: -320,
 				date: '2024-12-25',
-				description: 'Restaurant payment'
+				description: 'Restaurant payment',
+				category: 'entertainment'
 			},
 			{
 				source: 'transfer',
 				amount: 5400,
 				date: '2024-12-25',
-				description: 'Payment from Jemi Wilson'
+				description: 'Payment from Jemi Wilson',
+				category: 'income'
 			},
 			{
 				source: 'card',
 				amount: -534,
 				date: '2024-12-25',
-				description: 'Wallmart payment'
+				description: 'Wallmart payment',
+				category: 'other'
 			},
 			{
 				source: 'paypal',
 				amount: 2500,
 				date: '2024-12-25',
-				description: 'Deposit from PayPal'
+				description: 'Deposit from PayPal',
+				category: 'income'
 			},
 			{
 				source: 'card',
 				amount: -833,
 				date: '2024-12-25',
-				description: 'Restaurant payment'
+				description: 'Restaurant payment',
+				category: 'entertainment'
 			},
 			// 26 December
 			{
 				source: 'card',
 				amount: -850,
 				date: '2024-12-26',
-				description: 'ATM Withdrawal'
+				description: 'ATM Withdrawal',
+				category: 'other'
 			},
 			// 27 Decemeber
 			{
 				source: 'transfer',
 				amount: 419,
 				date: '2024-12-27',
-				description: 'Payment from Louis Risso'
+				description: 'Payment from Louis Risso',
+				category: 'income'
 			},
 			{
 				source: 'card',
 				amount: -2500,
 				date: '2024-12-27',
-				description: 'ATM Withdrawal'
+				description: 'Bitcoins',
+				category: 'investment'
 			},
 			{
 				source: 'transfer',
 				amount: 507,
 				date: '2024-12-27',
-				description: 'Payment from Jemi Wilson'
+				description: 'Payment from Jemi Wilson',
+				category: 'income'
 			},
 			{
 				source: 'transfer',
 				amount: 790,
 				date: '2024-12-27',
-				description: 'Payment from Allan Rickman'
+				description: 'Payment from Allan Rickman',
+				category: 'income'
 			},
 			// 28 December
 			{
 				source: 'paypal',
 				amount: 5621,
 				date: '2024-12-28',
-				description: 'Deposit from PayPal'
+				description: 'Deposit from PayPal',
+				category: 'income'
 			},
 			{
 				source: 'transfer',
 				amount: -1563,
 				date: '2024-12-28',
-				description: 'Payment to Mick Jacobs'
+				description: 'Payment to Mick Jacobs',
+				category: 'other'
 			},
 			// 29 Decemeber
 			{
 				source: 'paypal',
 				amount: 3963,
 				date: '2024-12-29',
-				description: 'Deposit from PayPal'
+				description: 'Deposit from PayPal',
+				category: 'income'
 			},
 			{
 				source: 'card',
 				amount: -1816,
 				date: '2024-12-29',
-				description: 'Restaurant payment'
+				description: 'Restaurant payment',
+				category: 'entertainment'
 			},
 			{
 				source: 'card',
 				amount: -6238,
 				date: '2024-12-29',
-				description: 'Jewelry payment'
+				description: 'Jewelry payment',
+				category: 'investment'
 			},
 			// 30 Decemeber
 			{
 				source: 'transfer',
 				amount: -856,
 				date: '2024-12-30',
-				description: 'Payment to Allan Rickman'
+				description: 'Payment to Allan Rickman',
+				category: 'other'
 			},
 			{
 				source: 'transfer',
 				amount: -533,
 				date: '2024-12-30',
-				description: 'Payment to Jemi Wilson'
+				description: 'Payment to Jemi Wilson',
+				category: 'other'
 			},
 			{
 				source: 'card',
 				amount: -48,
 				date: '2024-12-30',
-				description: 'Movie tickets payment'
+				description: 'Movie tickets payment',
+				category: 'entertainment'
 			},
 			{
 				source: 'paypal',
 				amount: 2354,
 				date: '2024-12-30',
-				description: 'Deposit from PayPal'
+				description: 'Deposit from PayPal',
+				category: 'income'
 			},
 		])
 	}),
+
+	// Mock endpoint for categories
+	http.get( '/api/categories', () => {
+		return HttpResponse.json([
+			{
+				id: 'bill',
+				label: 'Bill Expense'
+			},
+			{
+				id: 'entertainment',
+				label: 'Entertainment'
+			},
+			{
+				id: 'income',
+				label: 'Incomes'
+			},
+			{
+				id: 'investment',
+				label: 'Investment'
+			},
+			{
+				id: 'other',
+				label: 'Others'
+			},
+		])
+	})
 ]
