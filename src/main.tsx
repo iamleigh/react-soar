1eda2ed8d544fd362acf02f93c1d916f3e018095
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-03 16:34:28 -0500

    fix: Remove tailwindcss support

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import "./assets/css/tailwind.css"
 import "./assets/scss/main.scss";
 import "./assets/scss/soaricons.scss";
 import App from './App.tsx'
