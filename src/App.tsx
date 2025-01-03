60fc068f2e35db86cde00d920508a38a16a8fe1f
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-03 00:24:33 -0500

    fix: Install Vercel speed insights

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import Dashboard from './ui/pages/Dashboard'
 import Settings from './ui/pages/Settings'
 import NotFound from './ui/pages/404'
 
 const App: React.FC = () => {
 	return (
-		<Routes>
-			<Route path="/" element={<Dashboard />} />
-			<Route path="/settings" element={<Settings />} />
-			<Route path="*" element={<NotFound />} />
-		</Routes>
+		<>
+			<Routes>
+				<Route path="/" element={<Dashboard />} />
+				<Route path="/settings" element={<Settings />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			<SpeedInsights />
+		</>
 	)
 }
 
