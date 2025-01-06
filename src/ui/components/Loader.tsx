a804e3ed11f2eb9fc2d5b65dcbf0b63eff6566fa
Leighton Quito <2328848+iamleigh@users.noreply.github.com>
2025-01-06 16:28:57 -0500

    feat(loader): Create new component

---
+++ src/ui/components/Loader.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
+import { global, color, spacing, font } from '@helper/tokens'
+import spinner from '@image/spinner.svg'
+
+const spin = keyframes`
+	0% {
+		transform: rotate(0deg);
+	}
+	100% {
+		transform: rotate(360deg);
+	}
+`
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	background: ${color.secondary.light};
+	text-align: center;
+`
+
+const Spinner = styled.img`
+	width: 50px;
+	flex: 0 0 auto;
+	margin-bottom: ${spacing.vertical.sm}px;
+	animation: ${spin} 1s linear infinite;
+`
+
+const Text = styled.p<{ $bold?: boolean }>`
+	margin-bottom: ${spacing.vertical.sm/2}px;
+	color: ${color.primary.dark};
+	${props => props.$bold && 'font-weight: bold;'}
+	${props => props.$bold && 'font-size: ' + font.size.title.sm + ';'}
+
+	&:last-child {
+		margin-bottom: 0;
+	}
+
+	${global.bp.tablet} {
+		${props => props.$bold && 'font-size: ' + font.size.title.md + ';'}
+	}
+`
+
+interface LoaderProps {
+	title: string
+	message?: string
+}
+
+export const Loader: React.FC<LoaderProps> = ({ title, message }) => {
+	return (
+		<Container>
+			<Spinner src={ spinner } alt="Loading spinner" />
+			{ title && <Text $bold={ true }>{ title }</Text> }
+			{ message && <Text>{ message }</Text> }
+		</Container>
+	)
+}
